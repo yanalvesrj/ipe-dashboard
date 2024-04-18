bd5797445474e243119ff2d32d52922d412a8219
Rogger Samuel <rogger1023@hotmail.com>
2024-04-18 19:54:16 -0300

    projeto_laravel

---
+++ database/migrations/2024_04_18_011226_create_usuarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->unsignedBigInteger('codigo_usuario')->primary();
+            $table->string('ambiente'); 
+            $table->string('usuario');
+            $table->string('senha');
+            $table->string('regexcluida');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
